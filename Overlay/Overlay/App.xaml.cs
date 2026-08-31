8357915277175802acefcaec56b4916e041ddbe1
Jacob Pledge <jacob.pledge@gmail.com>
2026-08-31 02:00:01 +0100

    serving html in the background

---
+++ Overlay/Overlay/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Overlay.Services;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Overlay
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            OverlayServer.Start();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            OverlayServer.Stop();
+            base.OnExit(e);
+        }
+    }
+
+}
---
+++ Overlay/Overlay/Services/OverlayServer.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Text;
+using Microsoft.Extensions.FileProviders;
+
+namespace Overlay.Services
+{
+    public static class OverlayServer
+    {
+        private static WebApplication? _webApp;
+
+        public static async void Start()
+        {
+            var builder = WebApplication.CreateBuilder();
+            _webApp = builder.Build();
+
+            string wwwrootPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "wwwroot");
+
+            if (!Directory.Exists(wwwrootPath))
+            {
+                Directory.CreateDirectory(wwwrootPath);
+            }
+
+            _webApp.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(wwwrootPath),
+                RequestPath = ""
+            });
+
+            await _webApp.RunAsync("http://localhost:5000");
+        }
+
+        public static async void Stop()
+        {
+            if (_webApp != null ) 
+            {
+                await _webApp.StopAsync();
+                await _webApp.DisposeAsync();
+            }
+        }
+    }
+}
